fe116e226bff06e8f982c5ac6143a93e1102ef51
Jeff Linwood <jlinwood@gmail.com>
2023-10-06 18:11:57 -0500

    Add health check path

--- main.py
+++ main.py
@@ -7,7 +7,11 @@ app = Flask(__name__)
 def index():
     ij = imagej.init("2.5.0")
     print(ij.getVersion())
-    return f'{ij.getVersion()}'
+    return "Hello World!"
+
+@app.route('/health')
+def health():
+    return "OK"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=3000)
